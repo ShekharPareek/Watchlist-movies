d28b2be1bd146c19c4d34ffd3c9386c021337114
Shekhar <108127963+ShekharPareek@users.noreply.github.com>
2024-06-12 17:15:56 +0530

    Update Register.jsx
    btn color change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -49,7 +49,7 @@ function RegisterEmail() {
                         </div>
                         <div className="w-full md:w-full px-3 mb-6">
                             <button
-                                className="appearance-none block w-full bg-blue-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500"
+                                className="appearance-none block w-full bg-red-600 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500"
                                 type="submit"
                             >
                                 Sign in
